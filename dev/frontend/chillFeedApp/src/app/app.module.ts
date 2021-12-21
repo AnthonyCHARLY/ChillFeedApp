61124c851f8df374debd60c824fc8770ab6367c7
yassir daoudi <yassir.daoudi@etu.u-bordeaux.fr>
2021-12-21 12:19:49 +0100

    ajout route

--- dev/frontend/chillFeedApp/src/app/app.module.ts
+++ dev/frontend/chillFeedApp/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+const appRoutes: Routes =[
+  { path: 'login', component :LogInComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
