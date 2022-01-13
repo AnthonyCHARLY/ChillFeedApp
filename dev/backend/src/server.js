49d701db6398b00398f91281ac95adb1046ac913
Hassan Timli <htimli@emi.u-bordeaux.fr>
2022-01-13 12:00:32 +0100

    add static files to server

--- dev/backend/src/server.js
+++ dev/backend/src/server.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}) );
 
+app.use(express.static(process.cwd()+"../../frontend/chillFeedNutrition/dist/"));
+
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
