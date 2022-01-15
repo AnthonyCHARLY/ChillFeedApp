9e4e1518793c6561db1af17e1e7f36903effa1be
yassir daoudi <yassir.daoudi@etu.u-bordeaux.fr>
2022-01-15 19:39:38 +0100

    update curve

--- dev/frontend/chillFeedNutrition/src/app/clients-components/client-curve/client-curve.component.ts
+++ dev/frontend/chillFeedNutrition/src/app/clients-components/client-curve/client-curve.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import * as Chartist from 'chartist';
+import { CustomerService } from 'app/services/customer.service';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -10,28 +12,66 @@ import * as Chartist from 'chartist';
 })
 export class ClientCurveComponent implements OnInit {
 
-  dataDailySalesChart : any;
+  dataDailySalesChart= {
+    labels: [],
+    series: [ 
+        [] ]
+    
+  };
+ // dataDailySalesChart : any;
   evolutionWeight : any;
   curveObjectif : boolean;
   lastWeight : any;
   newWeight : any;
   valueWeight : number ;
 
-  constructor() {
-         this.curveObjectif = false;
-        this.dataDailySalesChart= {
-          labels: ['Week ', 'Week', 'Week'],
-          series: [ 
-              [12, 17, 7] ]
-          
-        };
-        this.curveObjectif=true;
-        this.valueWeight= 0;
+
+  currentClient: any;
+  currentClientSubscription: Subscription;
+ 
+
+  constructor(private customerServie: CustomerService) {
+    this.currentClientSubscription = this.customerServie.currentCustomerSubject.subscribe(
+      (customer: any) => {
+        this.currentClient = customer;
+
+        this.dataDailySalesChart.series[0].splice(0,this.currentClient.weightCurve.length);
+        this.dataDailySalesChart.labels.splice(0,this.currentClient.weightCurve.length);
+        this.ngCurveEvoluation();
+        console.log(" après supression : " + this.dataDailySalesChart.series[0]);
+
+        let ElementCurve=0;
+        let lenghtCurve =this.currentClient.weightCurve.length;
+        console.log(" taille tableau " +lenghtCurve);
+        console.log(" taille tableau " + this.currentClient.weightCurve);
+
+        this.currentClient.weightCurve.forEach(element => {
+
+          console.log("cure icic ===>" + element);
+          this.dataDailySalesChart.series[0].push(element);
+          console.log("cure icic ===>" + this.dataDailySalesChart.series[0]);
+          if(ElementCurve<lenghtCurve)
+            this.dataDailySalesChart.labels.push('Week');
+            
+            ElementCurve = ElementCurve +1;
+            console.log("cure icic ===>" + this.dataDailySalesChart.labels);
+        });
+
+        this.ngCurveEvoluation();
+    });
+    
+
+    this.curveObjectif = false;
+        
+    this.curveObjectif=true;
+    this.valueWeight= 0;
+        
    }
   
 
   
   ngOnInit(): void {
+    
     this.ngCurveEvoluation();
   }
 
@@ -54,7 +94,7 @@ export class ClientCurveComponent implements OnInit {
       this.curveObjectif = false;
       this.valueWeight= (this.newWeight - this.lastWeight);
     }
-    
+
     
     this.ngOnInit();
   }
@@ -66,8 +106,8 @@ export class ClientCurveComponent implements OnInit {
           lineSmooth: Chartist.Interpolation.cardinal({
               tension: 0
           }),
-          low: 0,
-          high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+          low: 45,
+          high: 100, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
           chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
       }
 
--- dev/frontend/chillFeedNutrition/src/app/services/auth.service.ts
+++ dev/frontend/chillFeedNutrition/src/app/services/auth.service.ts
@@ -25,6 +25,7 @@ export class AuthService implements CanActivate{
     state: RouterStateSnapshot): boolean {
 
     if (!this.isLoged)  {
+      
       alert('You are not connected');
       this.router.navigate(['log-in']);
       return false;
